b921593883da7f497ec1b05daf72ff00bac15b7f
Dani Carter <carte470@d.umn.edu>
2018-03-31 20:55:53 -0500

    server file working

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var request = require("request");
+var cheerio = require("cheerio");
+var express = require("express");
+
+//Initialize Express
+var app = express();
+
+//Require all models
+var db = require("../models");
+
+//A GET route for scraping the BuzzFeed News Website
+app.get("/", function(req, res) {
+	//First, we grab the body of the html with request
+	request("https://www.buzzfeed.com/news", function(error, response, html) {
+		//Then, we load that into cheerio and save it to $ for a shorthand select
+		var $ = cheerio.load(html);
+
+		//Now we grab every story-card tag, and do the following: 
+		$("div.story-card").each(function(i, element) {
+			//Save an empty result object
+			var result = {};
+
+			//Add the text and href of every link, and save them as properties of the result object
+			result.title = $(this)
+				.children("a")
+				.text();
+			result.link = $(this)
+				.children("a")
+				.attr("href");
+
+			//Create a new Headline using the 'result' object built from scraping
+			db.Headline.create(result)
+			.then(function(dbHeadline) {
+				//view the added result in the console
+				console.log(dbHeadline);
+			})
+			.catch(function(err) {
+				//If an error occured, send it to the client
+				return res.json(err);
+			});
+		});
+		//If scrape successful, save an Headline, send message to the client
+		res.send("Scrape Complete");
+	});
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -3,33 +3,35 @@ var express = require("express");
 var bodyParser= require("body-parser");
 var mongoose = require("mongoose");
 
+//Initialize Express
+var app = express();
 
-//Scraping tools
+//SCRAPING TOOLS
+//Makes HTTP request for the HTML page
+var request = require("request");
 var cheerio = require("cheerio");
 
 //Require all models
-var db = require(".models");
-
-var PORT = 3000;
+var db = require("./models");
 
 //Initialize Express
 var app = express();
 
 //CONGIGURE MIDDLEWARE
 //Use morgan logger for logging requests
-app.use(logger("dev"));
+// app.use(logger("dev"));
 //Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({extended:true}));
 //Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
 //Set Handlebars
-var exphbs = require("express.handlebars");
-app.enging("handlebars", exphbs({ defaultLayout: "main"}));
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-//Import routes and give server acess to them
-var routes = require("./routes");
+// Import routes and give server access to them
+var routes =require("./routes/index.js");
 app.use(routes);
 
 //If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
@@ -39,5 +41,13 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 //Conect to the MongoDB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
-	useMongoClient: true
+	// useMongoClient: true
+});
+
+//Set port
+var PORT = 3000;
+
+//Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
