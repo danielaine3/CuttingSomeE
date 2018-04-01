8d0864fbf200eb01bcbaa40f313ff0997c44e774
Dani Carter <carte470@d.umn.edu>
2018-04-01 12:27:20 -0500

    updated note model

--- models/Headline.js
+++ models/Headline.js
@@ -9,24 +9,27 @@ var HeadlineSchema = new Schema({
   // `title` is required and of type String
   title: {
     type: String,
-    required: true
+    required: true,
+    trim:true
   },
   // `link` is required and of type String
   link: {
     type: String,
-    required: true
+    required: true,
+    trim:true
   },
   pic: {
     type: String,
-    required: true
+    required: true,
+    trim:true
   }
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Headline with an associated Note
-  // note: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "Note"
-  // }
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
 });
 
 // This creates our model from the above schema, using mongoose's model method
