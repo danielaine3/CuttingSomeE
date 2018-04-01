d5b5399c3b99b5d5659701a3cef04af7fb606a4f
Dani Carter <carte470@d.umn.edu>
2018-04-01 12:04:02 -0500

    scrape working

--- models/Headline.js
+++ models/Headline.js
@@ -15,14 +15,14 @@ var HeadlineSchema = new Schema({
   link: {
     type: String,
     required: true
-  },
+  }
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Headline with an associated Note
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }
+  // note: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "Note"
+  // }
 });
 
 // This creates our model from the above schema, using mongoose's model method
--- routes/index.js
+++ routes/index.js
@@ -11,23 +11,22 @@ var db = require("../models");
 //A GET route for scraping the BuzzFeed News Website
 app.get("/", function(req, res) {
 	//First, we grab the body of the html with request
-	request("https://www.buzzfeed.com/news", function(error, response, html) {
+	request("http://www.eonline.com/news", function(error, response, html) {
 		//Then, we load that into cheerio and save it to $ for a shorthand select
 		var $ = cheerio.load(html);
 
 		//Now we grab every story-card tag, and do the following: 
-		$("div.story-card").each(function(i, element) {
+		$("h3").each(function(i, element) {
 			//Save an empty result object
 			var result = {};
 
 			//Add the text and href of every link, and save them as properties of the result object
 			result.title = $(this)
-				.children("a")
+				.children(".articleTitle")
 				.text();
 			result.link = $(this)
-				.children("a")
+				.children("a.articleTitle")
 				.attr("href");
-
 			//Create a new Headline using the 'result' object built from scraping
 			db.Headline.create(result)
 			.then(function(dbHeadline) {
@@ -42,6 +41,7 @@ app.get("/", function(req, res) {
 		//If scrape successful, save an Headline, send message to the client
 		res.send("Scrape Complete");
 	});
+
 });
 
 module.exports = app;
